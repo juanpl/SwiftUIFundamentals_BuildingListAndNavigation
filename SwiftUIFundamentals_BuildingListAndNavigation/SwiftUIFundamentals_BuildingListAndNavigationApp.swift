04a514d7e9e9370f4876d72013ee88e69a888502
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-10-04 08:59:28 -0500

    Initial Commit

---
+++ SwiftUIFundamentals_BuildingListAndNavigation/SwiftUIFundamentals_BuildingListAndNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFundamentals_BuildingListAndNavigationApp.swift
+//  SwiftUIFundamentals_BuildingListAndNavigation
+//
+//  Created by Juan Pablo Lasprilla Correa on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFundamentals_BuildingListAndNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
